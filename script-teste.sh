83391625da3cdf394893f3c45475e8b387de11ab
Matheus-Nasci <matheus.nasci2002@hotmail.com>
2022-11-10 21:36:14 -0300

    jar

--- script-teste.sh
+++ script-teste.sh
@@ -1,7 +1,6 @@
 #!/bin/bash
 
+wget https://github.com/Grupo8-2ADSB-TotemSystem/Jar-TotemSystem/raw/main/Totem_System-1.0-SNAPSHOT-jar-with-dependencies.jar
+sudo docker build -t Totem_System-1.0-SNAPSHOT-jar-with-dependencies.jar.
 sudo docker run -it --rm --name totem-system-java openjdk:11
-
-cd ~/Desktop
-cd Java_Totem_System/Totem_System/target
 java -jar Totem_System-1.0-SNAPSHOT-jar-with-dependencies.jar
--- script.sh
+++ script.sh
@@ -46,105 +46,108 @@ else
 		sudo docker-compose up -d
 		sudo docker start CONTAINER_TOTEMDB
 		sudo docker exec -it $(sudo docker ps -aqf "name=containerDB") mysql -u root -p -B -N -e "
-		create database totembd;
+			create database totembd;
 
-		use totembd;
+			use totembd;
 
-		create table endereco (
-		    idEndereco int primary key auto_increment,
-		    cep char(8) not null,
-		    numero int not null
-		);
+			create table estacao(
+    	idEstacao int primary key auto_increment,
+    	nomeEstacao varchar(45),
+    	cep char(8),
+    	numero int
+			);
 
-		create table estacao(
-			idEstacao int primary key auto_increment,
-		    fkEndereco int,
-			nomeEstacao varchar(45),
-		    foreign key (fkEndereco) references endereco(idEndereco)
-		);
+			create table empresa(
+    	idEmpresa int primary key auto_increment,
+    	fkEstacao int,
+    	nomeEmpresa varchar(45),
+    	cnpj char(14),
+    	foreign key (fkEstacao) references estacao(idEstacao)
+			);
 
-		create table empresa(
-		    idEmpresa int primary key auto_increment,
-		    fkEstacao int,
-		    nomeEmpresa varchar(45),
-		    cnpj char(14),
-		    foreign key (fkEstacao) references estacao(idEstacao)
-		);
+			create table usuario(
+    	idUsuario int auto_increment,
+    	fkEmpresa int,
+    	primary key (idUsuario, fkEmpresa),
+    	nomeUsuario varchar(45),
+    	email varchar(60),
+    	senha varchar(50),
+    	tipoUsuario int,
+    	foreign key (fkEmpresa) references empresa(idEmpresa)
+			);
 
-		create table usuario(
-		    idUsuario int auto_increment,
-		    fkEmpresa int,
-		    primary key (idUsuario, fkEmpresa),
-		    nomeUsuario varchar(45),
-		    email varchar(60),
-		    senha varchar(50),
-		    tipoUsuario int,
-		    foreign key (fkEmpresa) references empresa(idEmpresa)
-		);
+			create table totem (
+    	idTotem int primary key auto_increment,
+    	fkEstacao int,
+    	marca varchar (45),
+    	so varchar (45),
+    	foreign key (fkEstacao) references estacao(idEstacao)
+			);
 
-		create table totem (
-		    idTotem int primary key auto_increment,
-		    fkEstacao int,
-		    marca varchar (45),
-		    so varchar (45),
-		    foreign key (fkEstacao) references estacao(idEstacao)
-		);
+			create table disco (
+    	idDisco int auto_increment,
+    	fkTotem int,
+    	primary key (idDisco, fkTotem),
+    	nome varchar (45),
+    	modelo varchar (45),
+    	volumeTotal double,
+    	foreign key (fkTotem) references totem(idTotem)
+			);
 
-		create table disco (
-		    idDisco int auto_increment,
-		    fkTotem int,
-		    primary key (idDisco, fkTotem),
-		    nome varchar (45),
-		    modelo varchar (45),
-		    volumeTotal double,
-		    foreign key (fkTotem) references totem(idTotem)
-		);
+			create table memoria (
+    	idMemoria int primary key auto_increment,
+    	fkTotem int,
+    	memoriaTotal double,
+    	foreign key (fkTotem) references totem(idTotem)
+			);
 
-		create table memoria (
-		    idMemoria int primary key auto_increment,
-		    fkTotem int,
-		    memoriaTotal double,
-		    foreign key (fkTotem) references totem(idTotem)
-		);
+			create table processador (
+    	idProcessador int primary key auto_increment,
+    	fkTotem int,
+    	fabricante varchar (45),
+    	nome varchar (10),
+    	microArq varchar (10),
+    	frequencia double,
+    	foreign key (fkTotem) references totem(idTotem)
+			);
 
-		create table processador (
-		    idProcessador int primary key auto_increment,
-		    fkTotem int,
-		    fabricante varchar (45),
-		    nome varchar (10),
-		    microArq varchar (10),
-		    frequencia double,
-		    foreign key (fkTotem) references totem(idTotem)
-		);
+			create table processo (
+    	idProcesso int primary key auto_increment,
+    	fkTotem int,
+    	nome varchar(45),
+    	foreign key (fkTotem) references totem(idTotem)
+			);
 
-		create table processo (
-		    idProcesso int primary key auto_increment,
-		    fkTotem int,
-		    nome varchar(45),
-		    foreign key (fkTotem) references totem(idTotem)
-		);
+			create table dado (
+    	idDado int auto_increment,
+    	fkTotem int,
+    	primary key (idDado, fkTotem),
+    	memoriaUso double,
+    	memoriaDisponivel double,
+    	volumeUso double,
+    	volumeDisponivel double,
+    	memoriaUsoProcesso double,
+    	processadorUsoProcesso double,
+    	foreign key (fkTotem) references totem(idTotem)
+			);
 
-		create table dado (
-		    idDado int auto_increment,
-		    fkTotem int,
-		    primary key (idDado, fkTotem),
-		    memoriaUso double,
-		    memoriaDisponivel double,
-		    volumeUso double,
-		    volumeDisponivel double,
-		    memoriaUsoProcesso double,
-		    processadorUsoProcesso double,
-		    foreign key (fkTotem) references totem(idTotem)
-		);"
+			create table reporte (
+			idReporte int primary key auto_increment,
+			idTotemReporte varchar(45),
+			estacao varchar(45),
+			mensagem varchar(255),
+			fkEmpresa int,
+			fkTotem int,
+			foreign key (fkTotem) references totem(idTotem),
+			foreign key (fkEmpresa) references empresa(idEmpresa)
+			);"
 
-		echo Banco de dados Criado com Sucesso!
+			echo Banco de dados Criado com Sucesso!
 	fi
 fi
 
 sudo docker pull openjdk:11
-git clone https://github.com/Grupo8-2ADSB-TotemSystem/Java_Totem_System.git
+wget https://github.com/Grupo8-2ADSB-TotemSystem/Jar-TotemSystem/raw/main/Totem_System-1.0-SNAPSHOT-jar-with-dependencies.jar
 sudo docker build -t Totem_System-1.0-SNAPSHOT-jar-with-dependencies.jar.
 sudo docker run -it --rm --name totem-system-java openjdk:11
-
-cd Java_Totem_System/Totem_System/target
 java -jar Totem_System-1.0-SNAPSHOT-jar-with-dependencies.jar
