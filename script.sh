54e1269e1819a6267ee20716c6279fdd0a354713
Matheus-Nasci <matheus.nasci2002@hotmail.com>
2022-11-21 23:09:26 -0300

    caminho alterado

--- script.sh
+++ script.sh
@@ -42,6 +42,8 @@ else
 		pwd
 		sudo systemctl start docker
 		sudo systemctl enable docker
+		cd ..
+		cd ~/home/$USER/docker-mysql/
 		sudo docker-compose up -d
 		sudo docker start CONTAINER_TOTEMDB
 		sudo docker exec -it $(sudo docker ps -aqf "name=containerDB") mysql -u root -p -B -N -e "
